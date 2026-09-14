771eb52c66444c4932a563e9b178fe08848c5356
Tran Tai <taitq@mebisoft.vn>
2026-09-14 15:56:07 +0700

    hotfix(api): prepare deployment for Render

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,6 +13,8 @@ async function bootstrap() {
   });
   app.use(cookieParser());
 
+  app.setGlobalPrefix('api');
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
@@ -20,6 +22,7 @@ async function bootstrap() {
   const reflector = app.get(Reflector);
   app.useGlobalInterceptors(new ResponseInterceptor(reflector));
 
-  await app.listen(process.env.PORT ?? 3001);
+  const port = process.env.PORT || 3001;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
